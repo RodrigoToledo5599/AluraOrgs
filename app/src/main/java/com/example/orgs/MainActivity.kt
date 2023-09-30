2b52d7092db02e88f55b93bdc52108d0f7aa75e6
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-09-30 13:27:24 -0300

    all that i've done for now

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.example.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 
 class MainActivity : Activity(){
-
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        view.setText("VAI TOMAR NU CU ")
+        setContentView(R.layout.activity_main)
+    }
 
 }
